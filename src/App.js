82be9e69a9de8974fd3613c2e01e99716d224757
rishiiiiiiishah <142864064+rishiiiiiiishah@users.noreply.github.com>
2024-02-15 22:45:21 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./component/Navbar"
+import Navbar from "./component/Navbar";
+import Main from "./Main";
+
+function App() {
+  return (
+    <div>
+ <Navbar/>
+    <Main/>
+    </div>
+  
+  );
+}
+
+export default App;
---
+++ src/SingleQuestion.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {data} from "./data";
+
+export const Main = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SingleQuestion
